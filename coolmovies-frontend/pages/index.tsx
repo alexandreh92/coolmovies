cfddd7c7b3dc0e9836a1b2ea7ea94aba07dd2b39
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2022-06-27 19:17:26 -0300

    feat: style to frontend page

--- coolmovies-frontend/pages/index.tsx
+++ coolmovies-frontend/pages/index.tsx
@@ -1,21 +1,10 @@
 import { css } from "@emotion/react";
-import {
-  Button,
-  Container,
-  Grid,
-  Paper,
-  TextField,
-  Tooltip,
-  Typography,
-  Zoom,
-} from "@mui/material";
+import { Container, Link, Typography } from "@mui/material";
 import type { NextPage } from "next";
-import { useDispatch, useSelector } from "react-redux";
-import MovieCard from "../components/MovieCard";
+import { useDispatch } from "react-redux";
 import useUser from "../hooks/useUser";
-import { UserActions } from "../store/slices/user";
 
-const primary = "#1976d2";
+import { Link as NextLink } from "next/link";
 
 const Home: NextPage = () => {
   const dispatch = useDispatch();
@@ -24,8 +13,54 @@ const Home: NextPage = () => {
   console.log(user);
 
   return (
-    <Container maxWidth="sm">
-      <button onClick={() => dispatch(UserActions.getUser())}></button>
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        textAlign: "center",
+      }}
+    >
+      <Typography
+        sx={{ display: "flex" }}
+        variant="h3"
+        component="h1"
+        color="grey.800"
+      >
+        Hello
+        <Typography
+          sx={{ marginX: 2 }}
+          display="flex"
+          variant="h3"
+          component="h1"
+          fontWeight="bold"
+          color="primary"
+        >
+          {user.name}!
+        </Typography>
+      </Typography>
+
+      <div>
+        <Typography variant="h3" component="h1" color="grey.800">
+          Check out the latest
+        </Typography>
+
+        <Link
+          href="/reviews"
+          color="primary"
+          component={NextLink}
+          variant="h3"
+          fontWeight="bold"
+          underline="hover"
+          sx={{ cursor: "pointer" }}
+        >
+          Movie Reviews
+        </Link>
+      </div>
     </Container>
   );
 };
